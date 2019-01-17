71366225f99ec5728aa501f4c76afa5bd970ad5e
Marlon Moro <marlon.moro@gmail.com>
2019-01-17 00:12:03 -0200

    Reiniciando repostiorio

---
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
@@ -0,0 +1,93 @@
+package com.nelioalves.cursomc.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.dto.ClienteDTO;
+import com.nelioalves.cursomc.dto.ClienteNewDTO;
+import com.nelioalves.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.find(id);				
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		
+		// capturo todas as categorias e coloco no List chamado list
+		List<Cliente> list = service.findAll();
+		
+		//converto o list do tipo categoria em listDto do tipo ClienteDTO que tem como dados só nome e id
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	//metodo de paginaçao 
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome")String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC")String direction) {
+		
+		
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+}
+ 
---
+++ src/main/java/com/nelioalves/cursomc/services/AbastractEmailService.java
@@ -0,0 +1,75 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Date;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import com.nelioalves.cursomc.domain.Pedido;
+
+public abstract class AbastractEmailService implements EmailService {
+
+	@Value("${default.sender}")
+	private String sender;
+
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Override
+	public void sendOrderConfirmationEmail(Pedido obj) {
+		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(obj.getCliente().getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText(obj.toString());
+		return sm;
+	}
+
+	protected String htmlFromTemplatePedido(Pedido obj) {
+
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+
+		return templateEngine.process("email/confirmacaoPedido", context);
+	}
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {
+			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+		}
+		catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmH = new MimeMessageHelper(mimeMessage, true);
+		mmH.setTo(obj.getCliente().getEmail());
+		mmH.setFrom(sender);
+		mmH.setSubject("Pedido confirmado! Código: " + obj.getId());
+		mmH.setSentDate(new Date(System.currentTimeMillis()));
+		mmH.setText(htmlFromTemplatePedido(obj), true);
+		return mimeMessage;
+	}
+}
